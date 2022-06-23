b74cf08a7700f62896b5d1c7764b5d75a760a5bd
iuliantandaradev <108058836+iuliantandaradev@users.noreply.github.com>
2022-06-23 11:58:16 +0300

    test.
    test

---
+++ test.js
@@ -0,0 +1,7 @@
+echo "# testdev" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/iuliantandaradev/testdev.git
+git push -u origin main
